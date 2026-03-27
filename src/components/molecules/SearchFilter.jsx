7b6c1909ecb7f1b7236db4b80b1442147f156913
Harish T <tadka.harish54@gmail.com>
2026-03-27 07:26:05 +0530

    Update location name format in SearchFilter component
    - Changed the key for "Khopoli Pali Road" in the SearchFilter component to ensure consistent naming conventions.

--- src/components/molecules/SearchFilter.jsx
+++ src/components/molecules/SearchFilter.jsx
@@ -37,7 +37,7 @@ const Location = [
     { ranjanpada: "Ranjanpada" },
     { vindhane: "Vindhane" },
     { karjat: "Karjat" },
-    { khopoliPaliRoad: "khopoli-pali-road" },
+    { "khopoli-pali-road": "Khopoli Pali Road" },
 ];
 
 const SearchTrigger = (locationId) => {
